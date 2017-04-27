c0f1b21c107ba8c87efe2bf40e90ea5286e0482f
Evgeniy Skiba <Skiba.Evgeniy@hotmail.com>
2017-04-27 17:49:44 +0400

    initial commit

---
+++ LabWork3/Katalog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork3
+{
+    class Katalog
+    {
+        private String bookName;
+
+        public String BookName
+        {
+            get { return bookName; }
+            set { bookName = value; }
+        }
+        private String author;
+
+        public String Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        /// <summary>
+        /// default konstructor 
+        /// </summary>
+        public Katalog()
+        {
+
+        }
+
+        /// <summary>
+        /// constructor whit params 
+        /// </summary>
+        /// <param name="bookName"></param>
+        /// <param name="author"></param>
+        /// <param name="count"></param>
+        public Katalog(String bookName, String author, int count)
+        {
+            this.bookName = bookName;
+            this.author = author;
+            this.count = count;
+        }
+
+        public override string ToString()
+        {
+            String result = String.Format("BookName: {0}; Author: {1}  count : {2};",bookName,author,count);  
+            return result;
+        }
+    }
+
+}
---
+++ LabWork3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork3
+{
+    class Program
+    {
+       private static int firstRowNum = 2;
+       private static int secandRowNum = 3;
+
+        /*{
+            {book, book2},
+            {book3,book4,book5}
+        }*/
+
+       static  Katalog[][] arr = { new Katalog[firstRowNum], new Katalog[secandRowNum] };
+
+        static void Main(string[] args)
+        {
+
+            Katalog book = new Katalog("Block","the best",1);
+            Katalog book2 = new Katalog("Shindler", "the best", 5);
+
+            Katalog book3 = new Katalog("Gugo ", "Notre-Dame de Paris", 5);
+            Katalog book4 = new Katalog("Goethe's", "Faust", 3);
+
+            // first row
+            arr[0][0] = book;
+            arr[0][1] = book2;
+
+            //second row 
+            arr[1][0] = book3;
+            arr[1][1] = book4;
+
+            // arr[1][1] = book4;
+            arr[1][2] = new Katalog();
+
+            print();
+            arr[1][2] = createKetalogItem();
+            print();
+
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadLine();
+
+        }
+
+        private static void  print(){
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0} row", i + 1);
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.WriteLine(arr[i][j].ToString());
+                }
+            }
+        }
+
+        private static Katalog createKetalogItem()
+        {          
+
+            String author;
+            String bookName;
+            String countStr; 
+            int count;
+            Console.WriteLine("Enter the book name");
+            bookName = Console.ReadLine();
+
+            Console.WriteLine("Enter the autor name");
+            author = Console.ReadLine();
+
+            Console.WriteLine("Enter the count");
+            countStr = Console.ReadLine();
+            Int32.TryParse(countStr,out  count);
+
+            Katalog katalog = new Katalog(bookName, author, count);
+
+            return katalog;
+        }
+
+    }
+}
